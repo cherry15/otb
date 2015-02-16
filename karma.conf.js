d7b760023b746fdbeca8eaa3ec68d21fb53078f9
Cheryl Collier <cherylanncollier@gmail.com>
2015-02-16 16:45:16 +0000

    Added testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,57 +1,43 @@
 // Karma configuration
-// Generated on Sun Feb 15 2015 08:10:26 GMT+0000 (GMT Standard Time)
+// http://karma-runner.github.io/0.10/config/configuration-file.html
 
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
+    // base path, that will be used to resolve files and exclude
     basePath: '',
 
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    // testing framework to use (jasmine/mocha/qunit/...)
     frameworks: ['jasmine'],
-
 
     // list of files / patterns to load in the browser
     files: [
-      'components/**/*.js',
-      '/components/**/*.js',
-      '/components/**/*.js[3',
+      'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-resource/angular-resource.js',
+      'app/bower_components/angular-ui-router/release/angular-ui-router.js',
+      'app/app.js',
+      'app/holidays/*.js',
       'app/components/**/*.js',
-      'app/**/*.js',
-      'app/components/**/*.spec.js',
-      'app/**/*.spec.js'
+      'app/**/*.html',
+      'app/components/**/*.html'
     ],
 
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '**/*.html': 'html2js',
     },
 
+    ngHtml2JsPreprocessor: {
+      
+    },
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
+    // list of files / patterns to exclude
+    exclude: [],
 
     // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
+    port: 8080,
 
     // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     logLevel: config.LOG_INFO,
 
 
@@ -59,13 +45,19 @@ module.exports = function(config) {
     autoWatch: false,
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
+    // if true, it capture browsers, run tests and exit
     singleRun: false
   });
 };
